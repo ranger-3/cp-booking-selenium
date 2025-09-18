d4c89caff90341f11e6d13d1cdcaf446e8e5c6e4
Ian Murzac <ianik123450909w@gmail.com>
2025-09-18 03:08:27 +0300

    refactor(service-page): rename train_code to service_code (#1)

--- cp/pages/service_page.py
+++ cp/pages/service_page.py
@@ -6,16 +6,16 @@ from cp.pages.base_page import BasePage
 
 
 class ServicePage(BasePage):
-    def set_service(self, train_code: str):
+    def set_service(self, service_code: str):
         locator = (
             By.XPATH,
-            f"//span[@class='nobr' and normalize-space(text())='{train_code}']",
+            f"//span[@class='nobr' and normalize-space(text())='{service_code}']",
         )
 
         try:
             button = self.wait.until(EC.element_to_be_clickable(locator))
             button.click()
         except TimeoutException:
-            raise TimeoutException(f"Could not find train {train_code}")
+            raise TimeoutException(f"Could not find train {service_code}")
 
         return self
