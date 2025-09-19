ee68ca8fd258f70f3e9ab7b88865350e90209f11
Ian Murzac <ianik123450909w@gmail.com>
2025-09-19 11:17:11 +0300

    refactor(core): move DOCUMENT_TYPE and DISCOUNT_TYPE to constants.py

---
+++ cp/core/constants.py
@@ -0,0 +1,22 @@
+DOCUMENT_TYPE = {
+    "CITIZEN_CARD": 0,
+    "PASSPORT": 1,
+    "ID": 2,
+    "DRIVING_LICENSE": 3,
+    "RESIDENCE_CARD": 4,
+}
+
+DISCOUNT_TYPE = {
+    "FULL_PRICE": 0,
+    "CHILDREN": 1,
+    "YOUTH_25": 2,
+    "ACP": 3,
+    "YOUTH_CARD": 4,
+    "CP_INR_80": 5,
+    "CP_INR_80_COMPANION": 6,
+    "CP_INR_60_79": 7,
+    "SENIOR": 8,
+    "B2B": 9,
+    "TRADE_AGREEMENTS": 10,
+    "AZORES_AIRLINE": 11,
+}
